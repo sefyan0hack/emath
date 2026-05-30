de7662f1fdb7c9df5eb61de3f2003fdae0aac269
sefyan0hack <sefyan0hack@gmail.com>
2026-05-30 21:24:21 +0100

    add emath header

---
+++ emath/emath.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "free_func.hpp"
+#include "vec2.hpp"
+#include "vec3.hpp"
+#include "vec4.hpp"
+#include "ivec2.hpp"
+#include "ivec3.hpp"
+#include "ivec4.hpp"
+#include "mat2.hpp"
+#include "mat3.hpp"
+#include "mat4.hpp"
+#include "helpers.hpp"
