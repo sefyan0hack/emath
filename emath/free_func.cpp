0f2da553b16619a4197fbeeb75c7d173bc1fb7e6
sefyan0hack <sefyan0hack@gmail.com>
2026-05-31 02:48:14 +0100

    add `auto to_rad(vec3 const& deg) -> vec3`

--- emath/free_func.cpp
+++ emath/free_func.cpp
@@ -18,4 +18,9 @@ auto to_deg(const vec3& rad) -> vec3
     return {to_deg(rad.x), to_deg(rad.y), to_deg(rad.z)};
 }
 
+auto to_rad(const vec3 & deg) -> vec3
+{
+    return {to_rad(deg.x), to_rad(deg.y), to_rad(deg.z)};
+}
+
 }
--- emath/free_func.hpp
+++ emath/free_func.hpp
@@ -6,5 +6,6 @@ namespace emath {
 auto to_deg(float rad) -> float;
 auto to_deg(const vec3& rad) -> vec3;
 auto to_rad(float deg) -> float;
+auto to_rad(const vec3& deg) -> vec3;
 
 }
